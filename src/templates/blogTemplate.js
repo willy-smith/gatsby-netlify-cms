1b8d37431de85cd2195bd58492791a352c92b555
William Smith <wpsmith.dev@gmail.com>
2020-08-28 13:20:21 -0600

    add template for blog posts pulled from netlify

---
+++ src/templates/blogTemplate.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export default Template = ({ data }) => {
+  const { markdownRemark } = data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <div>
+      <div>
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
